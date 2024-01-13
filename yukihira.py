cb8a403c1018fb38aff8d6acc5738cfd804ed8c2
YukihiraSen <156506588+YukihiraSen@users.noreply.github.com>
2024-01-13 22:47:32 +0800

    Update yukihira.py

--- yukihira.py
+++ yukihira.py
@@ -1315,7 +1315,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/Mrchandu7/trick/blob/main/trick.txt").text
+	r1=requests.get("https://raw.githubusercontent.com/YukihiraSen/Yuki/main/approval.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
